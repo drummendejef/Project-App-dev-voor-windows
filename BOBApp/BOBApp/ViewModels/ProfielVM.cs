edff7106a41be1b551739e492772149ad434f92a
Kevin Van Lancker <kevin.van.lancker@student.howest.be>
2015-11-09 16:22:56 +0100

    ProfielVM
    Command bij profielvm aangemaakt + binding op de view.

--- BOBApp/BOBApp/ViewModels/ProfielVM.cs
+++ BOBApp/BOBApp/ViewModels/ProfielVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +10,19 @@ namespace BOBApp.ViewModels
     public class ProfielVM
     {
         //Properties
+        public RelayCommand AanpasCommand { get; set; }
 
         //Constructor
         public ProfielVM()
         {
-
+            AanpasCommand = new RelayCommand(Aanpassen);
         }
 
         //Methods
+        public void Aanpassen()
+        {
+
+        }
+
     }
 }
