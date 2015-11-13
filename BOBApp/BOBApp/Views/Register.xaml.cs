64a341278996456c14c40f320356816cce1e6528
Stijn Van Hulle <stijn.vanhulle@outlook.com>
2015-11-13 12:59:19 +0100

    Werkende versie, theme + splitview

--- BOBApp/BOBApp/Views/Register.xaml.cs
+++ BOBApp/BOBApp/Views/Register.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -26,51 +25,6 @@ namespace BOBApp.Views
         public Register()
         {
             this.InitializeComponent();
-        }
-
-        private void Akkoord_checked(object sender, RoutedEventArgs e)//Gebruiker heeft op de "akkoord" radiobutton geklikt.
-        {
-            //Alle mogelijke fouten overlopen.
-            if (textBoxNaam.Text.Length <= 2)
-            {
-                textBlockError.Text = "Vul een naam in";
-                Debug.WriteLine("Er staat niets in de naam textbox");
-                checkBoxAkkoordVoorwaarden.IsChecked = false;
-            }
-            else if (textBoxVoornaam.Text.Length <= 2)
-            {
-                textBlockError.Text = "Vul een Voornaam in";
-                Debug.WriteLine("Er staat niets in de textboxVoornaam");
-                checkBoxAkkoordVoorwaarden.IsChecked = false;
-            }
-            else if (textBoxEmail.Text.Length <= 5)
-            {
-                textBlockError.Text = "Vul een Emailadres in";
-                Debug.WriteLine("Er staat niets in de Email textbox");
-                checkBoxAkkoordVoorwaarden.IsChecked = false;
-            }
-            else if (textBoxGSMnr.Text.Length <= 4)
-            {
-                textBlockError.Text = "Vul een GSM nummer in";
-                Debug.WriteLine("Er staat niets in de GSM nummer");
-                checkBoxAkkoordVoorwaarden.IsChecked = false;
-            }
-            else
-            { 
-                buttonRegistreer.IsEnabled = true;
-                textBlockError.Text = "";
-            }
-
-
-
-#if DEBUG
-            Debug.WriteLine("Akkoord checkbox checked");
-            #endif
-        }
-
-        private void Akkoord_unchecked(object sender, RoutedEventArgs e)//Gebruiker heeft de "akkoord" radiobutton ontklikt.
-        {
-            buttonRegistreer.IsEnabled = false;
         }
     }
 }
