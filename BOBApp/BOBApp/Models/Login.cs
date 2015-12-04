a41702473c0033097275b8df1591ef6606d79db8
Stijn Van Hulle <stijn.vanhulle@outlook.com>
2015-12-04 10:02:59 +0100

    Revert "Update models"
    This reverts commit 282cd5079fecf82bf175994be05ec267d85b567d.

---
+++ BOBApp/BOBApp/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOBApp.Models
+{
+    public class Login
+    {
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Cellphone { get; set; }
+        public Boolean IsBob { get; set; }
+
+    }
+}
