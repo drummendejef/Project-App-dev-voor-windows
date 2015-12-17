99e4da7867e9922941bbfc3315284a0abe4c0e4b
Joren <jorenvandamme@hotmail.com>
2015-12-17 23:43:49 +0100

    Verzenden locatierequest json wordt opgevangen, moet nog uitlezen.

--- BOBApp/BOBApp/ViewModels/BestemmingenVM.cs
+++ BOBApp/BOBApp/ViewModels/BestemmingenVM.cs
@@ -3,12 +3,14 @@ using BOBApp.Views;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
+using Libraries;
 using Libraries.Models;
 using Libraries.Models.relations;
 using Libraries.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
@@ -22,6 +24,7 @@ namespace BOBApp.ViewModels
         private Task task;
         public RelayCommand GoDestinationCommand { get; set; }
         public RelayCommand AddDestinationCommand { get; set; }
+        public RelayCommand GoToCityCommand { get; set; }
 
         public List<City> Cities { get; set; }
         public List<Users_Destinations> Destinations { get; set; }
@@ -48,6 +51,7 @@ namespace BOBApp.ViewModels
             GetCities();
             AddDestinationCommand = new RelayCommand(AddDestination);
             GoDestinationCommand = new RelayCommand(GoDestination);
+            GoToCityCommand = new RelayCommand(TownToCoord);
             RaisePropertyChanged("SearchLocation");
             this.Destination = new City();
             RaisePropertyChanged("Destination");
@@ -88,6 +92,30 @@ namespace BOBApp.ViewModels
             this.Cities = await CityRepository.GetCities();
             this.Cities.Sort((a, b) => a.Name.CompareTo(b.Name));
 
+        }
+
+        //De naam van de gemeente omzetten naar de locatie
+        private async void TownToCoord()
+        {
+            //Naam ophalen uit de geselecteerde stad.
+            string Town = Destination.Name;
+
+            //URL aanmaken (tijdelijk, later in URL klasse zetten)
+            /*string bingkey = "dOUBDBVwN5QvZ1iHg90c~s2bgtqxiAZX20yceA6JFuw~An9qrmMutNOdQJ0PiF_t7WMqjN4lZBOWQaKrphjthrGdwmqvhjUvX8--_O2kP2K5";
+            string URLBasisTownToCoord = "http://dev.virtualearth.net/REST/v1/Locations?locality=";
+            string URLBingKey = "&key=";*/
+
+            //Volledige string maken
+            string URLFullTownToCoord = URL.BASISURLTOWNTOCOORD + Town + URL.URLBINGKEY + URL.BINGKEY;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var result = client.GetAsync(URLFullTownToCoord);
+                string json = await result.Result.Content.ReadAsStringAsync();
+
+            }
+
+            // https://msdn.microsoft.com/en-us/library/ff701714.aspx 
         }
     }
 }
