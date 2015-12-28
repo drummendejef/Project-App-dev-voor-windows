9d929e8c4dd1eb832d74a6d9af15c8b27b553075
Joren <jorenvandamme@hotmail.com>
2015-12-28 15:13:53 +0100

    Bob locatiestring naar point
    Locatiestring omzetten naar, voor de maps, begrijpbare locatie.

--- BOBApp/Libraries/Models/Bob.cs
+++ BOBApp/Libraries/Models/Bob.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 
 namespace Libraries.Models
 {
@@ -22,6 +23,23 @@ namespace Libraries.Models
             public User User { get; set; }
             public Bob Bob { get; set; }
             public string Location { get; set; }
+
+            private Geopoint geolocation;
+
+            public Geopoint GeoLocation
+            {
+                get
+                {
+                    string[] splittedcoord = Location.Split(',', ':', '}');
+                    BasicGeoposition tempbasic = new BasicGeoposition();
+                    tempbasic.Latitude = double.Parse(splittedcoord[1].ToString());
+                    tempbasic.Longitude = double.Parse(splittedcoord[3].ToString());
+                    geolocation = new Geopoint(tempbasic);
+                    return geolocation;
+                }
+                set { geolocation = value; }
+            }
+
         }
 
         public override string ToString()
