9253ec2c1ded897fd79e77c5b0032133c9aa6f0d
Joren <jorenvandamme@hotmail.com>
2015-12-30 14:19:02 +0100

    MailToConverter aangemaakt (en af?)

---
+++ BOBApp/BOBApp/Converters/MailToConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BOBApp.Converters
+{
+    public class MailToConverter : IValueConverter
+    {
+        //De hyperlinks die een emailadres bevatten.
+        //Als je wil dat die een mail stuurt, zet je daar mailto: voor.
+        //Maar dat werkt niet door databinding. 
+        //Dus deze converter zet er mailto: voorzetten.
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new Uri("mailto:" + value.ToString());
+        }
+
+        //ConvertBack niet geimplementeerd, oneway binding.
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
