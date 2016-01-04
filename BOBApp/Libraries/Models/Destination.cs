4d6dc86a826e1cff925faa793279b4ecd909e80c
Joren <jorenvandamme@hotmail.com>
2016-01-04 15:41:58 +0100

    Omzetten was toch niet nodig.

--- BOBApp/Libraries/Models/Destination.cs
+++ BOBApp/Libraries/Models/Destination.cs
@@ -12,23 +12,5 @@ namespace Libraries.Models
         public int ID { get; set; }
         public int Cities_ID { get; set; }
         public string Location { get; set; }
-
-        //Omzetten van de Locatiestring naar Geopoint om op de map te plaatsen
-        private Geopoint  geolocation;
-
-        public Geopoint GeoLocation
-        {
-            get
-            {
-                string[] splittedcoord = Location.Split(',', ':', '}');
-                BasicGeoposition tempbasic = new BasicGeoposition();
-                tempbasic.Latitude = double.Parse(splittedcoord[1].ToString());
-                tempbasic.Longitude = double.Parse(splittedcoord[3].ToString());
-                geolocation = new Geopoint(tempbasic);
-                return geolocation;
-            }
-            set { geolocation = value; }
-        }
-
     }
 }
