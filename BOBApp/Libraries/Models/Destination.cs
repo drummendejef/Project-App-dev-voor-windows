221bfa3aec087aefc64654aa5bbae1f157380840
Joren <jorenvandamme@hotmail.com>
2016-01-04 15:06:47 +0100

    Destination klaargemaakt voor locatie naar coordinaten

--- BOBApp/Libraries/Models/Destination.cs
+++ BOBApp/Libraries/Models/Destination.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 
 namespace Libraries.Models
 {
@@ -11,5 +12,23 @@ namespace Libraries.Models
         public int ID { get; set; }
         public int Cities_ID { get; set; }
         public string Location { get; set; }
+
+        //Omzetten van de Locatiestring naar Geopoint om op de map te plaatsen
+        private Geopoint  geolocation;
+
+        public Geopoint GeoLocation
+        {
+            get
+            {
+                string[] splittedcoord = Location.Split(',', ':', '}');
+                BasicGeoposition tempbasic = new BasicGeoposition();
+                tempbasic.Latitude = double.Parse(splittedcoord[1].ToString());
+                tempbasic.Longitude = double.Parse(splittedcoord[3].ToString());
+                geolocation = new Geopoint(tempbasic);
+                return geolocation;
+            }
+            set { geolocation = value; }
+        }
+
     }
 }
